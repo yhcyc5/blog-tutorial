025b9b54e77c1a5f222ed386d52eba8178a8682a
Zoe Chen <Zoe@Zoede-Air.Home>
2017-05-26 19:36:20 +0800

    測試上傳

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use View;
+use App\Post;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return View::make('home')
+            ->with('title', 'My Blog')
+            ->with('posts', $posts);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = Input::all();
+        return $input['title'];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return View::make('home')
+            ->with('title', '首頁')
+            ->with('hello', '大家好～～'.$id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{{ $title }}</title>
+</head>
+<body>
+<h1>{{ $title }}</h1>
+<div>{{ link_to('post/create', '新增') }}</div>
+@if (isset($posts))
+    <ol>
+        @foreach ($posts as $post)
+            <li>{{ link_to('post/'.$post->id, $post->title) }}
+                ({{ link_to('post/'.$post->id.'/edit', '編輯') }})</li>
+        @endforeach
+    </ol>
+@endif
+</body>
+</html>
