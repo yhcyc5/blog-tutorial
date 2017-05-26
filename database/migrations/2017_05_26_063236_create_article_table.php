6fa4107c2fece485e4494fcd9865566f1868f2da
Zoe Chen <Zoe@Zoede-Air.Home>
2017-05-27 01:22:58 +0800

    新增文章

---
+++ database/migrations/2017_05_26_063236_create_article_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title', 20);
+            $table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
